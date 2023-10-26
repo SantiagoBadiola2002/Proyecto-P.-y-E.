ad4aecfb48038f5a9b4abf6a7a2dc9b92cdcee46
SantiagoBadiola2002 <129560206+SantiagoBadiola2002@users.noreply.github.com>
2023-10-25 21:56:26 -0300

    Subida del codigo

---
+++ src/main/java/com/mycompany/projectoprobabilidadyestadistica/ProjectoProbabilidadYEstadistica.java
@@ -0,0 +1,13 @@
+package com.mycompany.projectoprobabilidadyestadistica;
+
+
+import presentacion.ventana;
+
+
+public class ProjectoProbabilidadYEstadistica {
+
+    public static void main(String[] args) {
+        ventana v1 = new ventana();
+        v1.setVisible(true);
+    }
+}
